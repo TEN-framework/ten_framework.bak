093b68507ed6eb051f6699f97480308d6ff815ab
Hu Yueh-Wei <wei.hu.tw@gmail.com>
2025-04-26 18:41:10 +0800

    fix: refine codes

--- core/include_internal/ten_rust/ten_rust.h
+++ core/include_internal/ten_rust/ten_rust.h
@@ -18,88 +18,79 @@ typedef struct MetricHandle MetricHandle;
 typedef struct ServiceHub ServiceHub;
 typedef struct ten_app_t ten_app_t;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+TEN_RUST_PRIVATE_API void ten_rust_free_cstring(const char *ptr);
 
-TEN_RUST_API void ten_rust_free_cstring(const char *ptr);
+TEN_RUST_PRIVATE_API Cipher *ten_cipher_create(const char *algorithm,
+                                               const char *params);
 
-TEN_RUST_API Cipher *ten_cipher_create(const char *algorithm,
-                                       const char *params);
+TEN_RUST_PRIVATE_API void ten_cipher_destroy(Cipher *cipher_ptr);
 
-TEN_RUST_API void ten_cipher_destroy(Cipher *cipher_ptr);
+TEN_RUST_PRIVATE_API bool ten_cipher_encrypt_inplace(Cipher *cipher_ptr,
+                                                     uint8_t *data,
+                                                     uintptr_t data_len);
 
-TEN_RUST_API bool ten_cipher_encrypt_inplace(Cipher *cipher_ptr, uint8_t *data,
-                                             uintptr_t data_len);
-
-TEN_RUST_API bool ten_interface_schema_resolve_definition(
+TEN_RUST_PRIVATE_API bool ten_interface_schema_resolve_definition(
     const char *content, const char *base_dir, const char **out_content,
     const char **out_err_msg);
 
-TEN_RUST_API char *ten_remove_json_comments(const char *json_with_comments);
+TEN_RUST_PRIVATE_API char *ten_remove_json_comments(
+    const char *json_with_comments);
 
-TEN_RUST_API bool ten_validate_manifest_json_string(const char *manifest_data,
-                                                    const char **out_err_msg);
+TEN_RUST_PRIVATE_API bool ten_validate_manifest_json_string(
+    const char *manifest_data, const char **out_err_msg);
 
-TEN_RUST_API bool ten_validate_manifest_json_file(const char *manifest_file,
-                                                  const char **out_err_msg);
+TEN_RUST_PRIVATE_API bool ten_validate_manifest_json_file(
+    const char *manifest_file, const char **out_err_msg);
 
-TEN_RUST_API bool ten_validate_property_json_string(const char *property_data,
-                                                    const char **out_err_msg);
+TEN_RUST_PRIVATE_API bool ten_validate_property_json_string(
+    const char *property_data, const char **out_err_msg);
 
-TEN_RUST_API bool ten_validate_property_json_file(const char *property_file,
-                                                  const char **out_err_msg);
+TEN_RUST_PRIVATE_API bool ten_validate_property_json_file(
+    const char *property_file, const char **out_err_msg);
 
-TEN_RUST_API ServiceHub *ten_service_hub_create(const char *telemetry_host,
-                                                uint32_t telemetry_port,
-                                                const char *api_host,
-                                                uint32_t api_port,
-                                                ten_app_t *app);
+TEN_RUST_PRIVATE_API ServiceHub *ten_service_hub_create(
+    const char *telemetry_host, uint32_t telemetry_port, const char *api_host,
+    uint32_t api_port, ten_app_t *app);
 
-TEN_RUST_API void ten_service_hub_shutdown(ServiceHub *service_hub_ptr);
+TEN_RUST_PRIVATE_API void ten_service_hub_shutdown(ServiceHub *service_hub_ptr);
 
-TEN_RUST_API const char *ten_get_runtime_version(void);
+TEN_RUST_PRIVATE_API const char *ten_get_runtime_version(void);
 
-TEN_RUST_API MetricHandle *ten_metric_create(ServiceHub *system_ptr,
-                                             uint32_t metric_type,
-                                             const char *name, const char *help,
-                                             const char *const *label_names_ptr,
-                                             uintptr_t label_names_len);
+TEN_RUST_PRIVATE_API MetricHandle *ten_metric_create(
+    ServiceHub *system_ptr, uint32_t metric_type, const char *name,
+    const char *help, const char *const *label_names_ptr,
+    uintptr_t label_names_len);
 
-TEN_RUST_API void ten_metric_destroy(MetricHandle *metric_ptr);
+TEN_RUST_PRIVATE_API void ten_metric_destroy(MetricHandle *metric_ptr);
 
-TEN_RUST_API void ten_metric_counter_inc(MetricHandle *metric_ptr,
-                                         const char *const *label_values_ptr,
-                                         uintptr_t label_values_len);
+TEN_RUST_PRIVATE_API void ten_metric_counter_inc(
+    MetricHandle *metric_ptr, const char *const *label_values_ptr,
+    uintptr_t label_values_len);
 
-TEN_RUST_API void ten_metric_counter_add(MetricHandle *metric_ptr, double value,
-                                         const char *const *label_values_ptr,
-                                         uintptr_t label_values_len);
-
-TEN_RUST_API void ten_metric_histogram_observe(
+TEN_RUST_PRIVATE_API void ten_metric_counter_add(
     MetricHandle *metric_ptr, double value, const char *const *label_values_ptr,
     uintptr_t label_values_len);
 
-TEN_RUST_API void ten_metric_gauge_set(MetricHandle *metric_ptr, double value,
-                                       const char *const *label_values_ptr,
-                                       uintptr_t label_values_len);
+TEN_RUST_PRIVATE_API void ten_metric_histogram_observe(
+    MetricHandle *metric_ptr, double value, const char *const *label_values_ptr,
+    uintptr_t label_values_len);
 
-TEN_RUST_API void ten_metric_gauge_inc(MetricHandle *metric_ptr,
-                                       const char *const *label_values_ptr,
-                                       uintptr_t label_values_len);
+TEN_RUST_PRIVATE_API void ten_metric_gauge_set(
+    MetricHandle *metric_ptr, double value, const char *const *label_values_ptr,
+    uintptr_t label_values_len);
 
-TEN_RUST_API void ten_metric_gauge_dec(MetricHandle *metric_ptr,
-                                       const char *const *label_values_ptr,
-                                       uintptr_t label_values_len);
+TEN_RUST_PRIVATE_API void ten_metric_gauge_inc(
+    MetricHandle *metric_ptr, const char *const *label_values_ptr,
+    uintptr_t label_values_len);
 
-TEN_RUST_API void ten_metric_gauge_add(MetricHandle *metric_ptr, double value,
-                                       const char *const *label_values_ptr,
-                                       uintptr_t label_values_len);
+TEN_RUST_PRIVATE_API void ten_metric_gauge_dec(
+    MetricHandle *metric_ptr, const char *const *label_values_ptr,
+    uintptr_t label_values_len);
 
-TEN_RUST_API void ten_metric_gauge_sub(MetricHandle *metric_ptr, double value,
-                                       const char *const *label_values_ptr,
-                                       uintptr_t label_values_len);
+TEN_RUST_PRIVATE_API void ten_metric_gauge_add(
+    MetricHandle *metric_ptr, double value, const char *const *label_values_ptr,
+    uintptr_t label_values_len);
 
-#ifdef __cplusplus
-}
-#endif
+TEN_RUST_PRIVATE_API void ten_metric_gauge_sub(
+    MetricHandle *metric_ptr, double value, const char *const *label_values_ptr,
+    uintptr_t label_values_len);
